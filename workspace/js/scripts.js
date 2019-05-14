a3d86c5c68bba5252bfb73813bd5bd35e1c432a2
Zsolt Király <hello@zsoltkiraly.com>
2019-05-14 15:18:10 +0200

    menu fixed patch-003 { switch from grunt to gulp }

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -3,7 +3,31 @@ Menu fixed - Code by Zsolt Király
 v1.0.5 - 2018-04-19
 */
 
+'use strict';
 var fixMenu = function() {
+
+    function signatura() {
+        if (window['console']) {
+            const text = {
+                black: '%c     ',
+                blue: '%c   ',
+                author: '%c  Zsolt Király  ',
+                github: '%c  https://zsoltkiraly.com/'
+            }
+    
+            const style = {
+                black: 'background: #282c34',
+                blue: 'background: #61dafb',
+                author: 'background: black; color: white',
+                github: ''
+            }
+    
+            console.log(text.black + text.blue + text.author + text.github, style.black, style.blue, style.author, style.github);
+        }
+    }
+    
+    signatura();
+
     var scrollPos = 0;
 
     function setMenuHeight(c, m) {
